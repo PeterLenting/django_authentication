dbf090132d0b64f03011c7ed852f2086287e6de2
PeterLenting <pplenting@gmail.com>
2019-11-28 10:14:22 +0000

    added registration form and functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
 def index(request):
@@ -20,13 +20,14 @@ def logout(request):
 def login(request):
     """Return a login page"""
     if request.user.is_authenticated:
+        print("You are already Logged in")
         return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
-                                    password=request.POST['password'])
+                                     password=request.POST['password'])
             messages.success(request, "You have successfully logged in!")
 
             if user:
@@ -37,3 +38,31 @@ def login(request):
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
+
+
+def registration(request):
+    """Render the registration page"""
+    ###  Only called on accessing the page via a URL, not from registering
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+        print("You are already Logged in")
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+
+        if registration_form.is_valid():
+            registration_form.save()
+
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+            if user:
+                auth.login(user=user, request=request)
+                #    This is what happens if you register.  We just send a message to the screen saying that the person has registered.
+                messages.success(request, "You have successfully registered")
+                # return redirect(reverse('index'))
+                return redirect(reverse('index'))
+            else:
+                messages.error(request, "Unable to register your account at this time")
+    else:
+        registration_form = UserRegistrationForm()
+    return render(request, 'registration.html', {
+        "registration_form": registration_form})
